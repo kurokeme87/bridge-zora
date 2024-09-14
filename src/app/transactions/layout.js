e2ef7c55f84a5e1e8a701dae56e69d14c3bfab56
AdamJamiu <adamjamiu98@gmail.com>
2024-09-14 17:32:37 +0100

    Update layout.js

--- src/app/transactions/layout.js
+++ src/app/transactions/layout.js
@@ -28,9 +28,5 @@
 // });
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
+  return <div>{children}</div>;
 }
